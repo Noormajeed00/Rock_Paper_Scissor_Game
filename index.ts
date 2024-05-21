181aa55fedf008dfa3af48b550b75a10637c99f4
— NooR ShaikH <160132632+Noormajeed00@users.noreply.github.com>
2024-05-21 15:07:52 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,15 @@
+// IF ELSE STATEMENT USING ROCK PAPER SCISSOR GAME 
+
+let player1: string = "Rock"
+let player2: string = "Paper"
+
+if (player1 === player2) {
+    console.log("its a Tie!")
+}else if ((player1 === "Rock" && player2 === "Scissor") ||
+ (player1 === "Scissor" && player2 === "Paper") ||
+ (player1 === "Paper" && player2 === "Rock")) {
+    console.log("player 1 win this match")
+ } else{
+    console.log("player 2 win this match successfully!");
+ };
+
